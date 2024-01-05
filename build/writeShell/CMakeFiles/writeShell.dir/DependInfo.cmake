05be11a6a362bfdb24f313fb9fdfbd514e90f276
FXD-jl <fxdjby603@gmail.com>
2024-01-05 08:59:05 +0800

    test-1.5

---
+++ build/writeShell/CMakeFiles/writeShell.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/jl/Documents/Alogrithm/writeShell/src/main.c" "writeShell/CMakeFiles/writeShell.dir/src/main.c.o" "gcc" "writeShell/CMakeFiles/writeShell.dir/src/main.c.o.d"
+  "/Users/jl/Documents/Alogrithm/writeShell/src/writeShell.c" "writeShell/CMakeFiles/writeShell.dir/src/writeShell.c.o" "gcc" "writeShell/CMakeFiles/writeShell.dir/src/writeShell.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
