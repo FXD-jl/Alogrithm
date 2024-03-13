3bdda2819d2bc5a27482cfa6fc9a9820075c07ed
FXD-jl <fxdjby603@gmail.com>
2024-03-13 09:30:58 +0800

    2024-3-13

---
+++ GtkStudy/build/CMakeFiles/gtkStudy.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gtkStudy.dir/src/main.c.o"
+  "CMakeFiles/gtkStudy.dir/src/main.c.o.d"
+  "gtkStudy"
+  "gtkStudy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gtkStudy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ gale_shaple/src/gale_shapley.c
@@ -0,0 +1,6 @@
+#include"gale_shapley.h"
+#include<collectc/cc_array.h>
+int main(){
+  
+  
+}
